b3550be1c7c8de6ba3c11c70b289b145cb3ace15
Jack Zhou <jack.zhou@intel.com>
2019-09-13 13:28:38 -0700

    remove macros:
    -CDEF_AVX_OPT

--- Source/Lib/Common/Codec/EbDefinitions.h
+++ Source/Lib/Common/Codec/EbDefinitions.h
@@ -50,7 +50,6 @@ extern "C" {
     // MACROS TO REMOVE
 #define INCOMPLETE_SB_FIX                 1 // Handle the incomplete SBs properly based on the standard and consider all allowed blocks
 #define QPS_TUNING                        1 // Tune the QPS algorithm to consider ALR_REF filtering and movement of the pictures
-#define CDEF_AVX_OPT                      1
 
 //FOR DEBUGGING - Do not remove
 #define NO_ENCDEC                         0 // bypass encDec to test cmpliance of MD. complained achieved when skip_flag is OFF. Port sample code from VCI-SW_AV1_Candidate1 branch
